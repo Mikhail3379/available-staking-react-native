228bc39b97c41d41d8f858d2d0ad938ef1745fce
Mikhail Bakushev <78318602+Mikhail3379@users.noreply.github.com>
2022-06-28 14:43:56 +0100

    Update Available.tsx

--- Available.tsx
+++ Available.tsx
@@ -2,116 +2,116 @@ import React, { memo, useEffect, useState } from "react";
 import { Navigation } from "../types";
 import { VictoryPie } from "victory-native";
 
-import { View, Text , useWindowDimensions } from "react-native";
+import { View, Text, useWindowDimensions, Dimensions, StyleSheet } from "react-native";
 import Button from "./Button";
+import AppButton from "./AppButton";
+const { width, height } = Dimensions.get('window');
 
 type Props = {
-    navigation: Navigation;
-  };
+  navigation: Navigation;
+};
 
-  const graphicData = [
-    { y: 25, x: "" },
-    { y: 15, x: ''},
-    { y: 10, x: ''},
-    { y: 30, x: ''},
-    { y: 20, x: ''},
-    ]
+const graphicData = [
+  { y: 25, x: '' },
+  { y: 15, x: '' },
+  { y: 10, x: '' },
+  { y: 30, x: '' },
+  { y: 20, x: '' },
+]
 
-    const graphicColor = ['purple', 'green', 'blue', 'brown', 'yellow']
+const graphicColor = ['#965FD7', '#7EFBC1', '#268FFF', '#F09242', '#F9D208']
 
-   
 
-    const Available = () => (
-        <><View style={{
-           position: 'absolute',
-           top: 15,
-           left: '5%',
-         }}>
-           <VictoryPie
-             // @ts-ignore
-             data={graphicData}
-             width={350}
-             height={350}
-             innerRadius={110}
-             style={{
-               labels: {
-                 fill: 'white', fontSize: 1, padding: 7,
-               },
-             }}
-             colorScale={graphicColor}
-             padAngle={0}
-             endAngle={-90}
-             startAngle={90} />
-         </View><Text
-           style={{
-             position: 'absolute',
-             bottom: 85,
-             left: '15%',
-             color: 'black',
-           }}
-         ><View
-           style={{
-             backgroundColor: "green",
-             height: 20,
-             width: 20,
-             borderRadius: 10,
-           }}
-    
-         ></View> &nbsp; 32RAY  &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;<View
-               style={{
-                 backgroundColor: "yellow",
-                 height: 20,
-                 width: 20,
-                 borderRadius: 10,
-               }}
-    
-             ></View>&nbsp; 25NOVA &nbsp; &nbsp; &nbsp;
-             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-             &nbsp;
-             <View
-               style={{
-                 backgroundColor: "blue",
-                 height: 20,
-                 width: 20,
-                 borderRadius: 10,
-               }}
-             ></View> &nbsp; 897USDC &nbsp; &nbsp;&nbsp;&nbsp;
-             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-             <View
-               style={{
-                 backgroundColor: "purple",
-                 height: 20,
-                 width: 20,
-                 borderRadius: 10,
-               }}
-    
-             ></View>&nbsp;64SOL &nbsp;  &nbsp; &nbsp; &nbsp;
-             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-             <View
-               style={{
-                 backgroundColor: "brown",
-                 height: 20,
-                 width: 20,
-                 borderRadius: 10,
-               }}
-    
-             ></View> 0.0004BTC </Text><Button
-               color="whitesmoke"
-               background-color="linear-gradient(91.87deg, #4500FF 0%, #38FFBD 100%)"
-               style={{
-                 position: 'absolute',
-                 
-                 bottom: 0,
-                 left: 15,
-               }}>Buy crypto</Button><Text style={{
-                 position: 'absolute',
-                 bottom: 195,
-                 left: '45%',
-                 color: 'black',
-               }}> $1,236.89 </Text></>
-      );
 
-        export default memo(Available);
+const Available = () => {
+  const [myBalance, setMyBalance] = useState(1.236);
+  
+  return(
+  <View style={{
+    //position: 'absolute',
+    top: 25,
+    //backgroundColor:"red",
+    alignItems: "center",
+    //borderColor:'red',borderWidth:1 
+    //left: '5%',
+  }}>
+    <VictoryPie
+      // @ts-ignore
+      data={graphicData}
+      width={350}
+      height={350}
+      innerRadius={110}
+      style={{
+        labels: {
+          fill: 'white', fontSize: 1, padding: 7,
+        },
+      }}
+      colorScale={graphicColor}
+      padAngle={0}
+      endAngle={-120}
+      startAngle={120}
+    />
+    <View style={{
+    position: 'absolute',
+    alignItems: "center",
+    top:150
+  
+  }}>
+    <Text style={{
+      color: 'black',
+      fontWeight: '400',
+      fontSize: 18,
+      fontStyle: "normal",
+      lineHeight: 30
+      
+    }}> ${myBalance} </Text>
+</View>
+<View style={{
+    position: 'absolute',
+    alignItems: "center",
+    top:height < 700 ? 300 :350,
+    //backgroundColor:"red"
+  
+  }}>
+    <Text style={{
+      color: '#808080',
+      fontWeight: '300',
+      fontSize: 18,
+      fontStyle: "normal",
+      lineHeight: 30,
+      textAlign:"center"
+      
+    }}> Add assets to your wallet to see this chart fill up with pretty colours! </Text>
+</View>
+
+    <Button
+        color="whitesmoke"
+        background-color="linear-gradient(91.87deg, #4500FF 0%, #38FFBD 100%)"
+        style={{
+          position: 'absolute',
+
+          bottom: 0,
+          left: 15,
+        }}>Buy crypto</Button>
+  </View>
+
+);
+      }
+      const styles = StyleSheet.create({
+        container: {
+          flex: 1,
+          paddingHorizontal: 20,
+          paddingVertical:height < 800 ? 2 :20
+        },
+        cadreRow: {
+        
+        },
+        textCondition: {
+      
+        },
+        textWhy: {
+         
+        },
+        
+      });
+export default memo(Available);
